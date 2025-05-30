852415706d370bacb942a13e95d862b7528c7291
MegamationsTheAnimator <matiasferreiradj@gmail.com>
2025-05-30 09:09:21 +0100

    gng

--- Cenas do David/08-DavidFerreira-ProjetoFinal/obj/Debug/net8.0-windows/08-DavidFerreira-ProjetoFinal.AssemblyInfo.cs
+++ Cenas do David/08-DavidFerreira-ProjetoFinal/obj/Debug/net8.0-windows/08-DavidFerreira-ProjetoFinal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("08-DavidFerreira-ProjetoFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2f7fa57b43f6e77fbfd81be63190cac08212b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+449643decee81031c27c1bcac89c99f68d5c7103")]
 [assembly: System.Reflection.AssemblyProductAttribute("08-DavidFerreira-ProjetoFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("08-DavidFerreira-ProjetoFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
